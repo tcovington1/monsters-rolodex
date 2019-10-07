3ee5cad42e293f2dd04b985fbb6b0a1fae6b2615
Taylor Covington <tcovingtondev@gmail.com>
2019-10-06 21:59:04 -0600

    Updates

---
+++ precache-manifest.103011931b2d5b539eeab5095b805ecc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c83122a0825774440044c48fb98af73",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bfbe63c550cc12f71979",
+    "url": "/monsters-rolodex/static/css/main.c8426214.chunk.css"
+  },
+  {
+    "revision": "e57ff400467a2f25bbe9",
+    "url": "/monsters-rolodex/static/js/2.1a8f040e.chunk.js"
+  },
+  {
+    "revision": "bfbe63c550cc12f71979",
+    "url": "/monsters-rolodex/static/js/main.32fbbf42.chunk.js"
+  },
+  {
+    "revision": "c4a009f5d3a5fc9c4346",
+    "url": "/monsters-rolodex/static/js/runtime-main.205b164d.js"
+  }
+]);

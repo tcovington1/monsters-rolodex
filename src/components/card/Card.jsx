1a7fac17d365428d2f9ab476bda620e7a88bb5dd
Taylor Covington <tcovingtondev@gmail.com>
2019-10-06 22:00:22 -0600

    added production build for github pages

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,7 +6,7 @@ const Card = (props) => (
     <img alt='monster' 
     src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} 
     />
-    <h1>{props.monster.name}</h1>
+    <h2>{props.monster.name}</h2>
     <h3>{props.monster.email}</h3>
   </div>
 )
